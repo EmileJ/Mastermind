00f6c99cd348e4183e447c25ea87bf92208a8951
EmileJ <72444841+EmileJ@users.noreply.github.com>
2020-11-26 16:05:01 +0100

    started working on drag and drop of pins

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //const code = {}
-const colors = 4
+const colors = ['red','green','blue','yellow']
 var turn = 1;
 var code = [];
 
 const codePin = '&#9864;';
-const indicator = '&#128205;';
+const indicator = '&#x128205;';
 
 var cell = document.createElement('TD')
 var row = document.createElement('TR')
@@ -13,7 +13,13 @@ const table = document.getElementById('playField')
 var test = 0;
 
 for (let i = 0; i < 5; i++) {
-    row.append(cell.cloneNode())
+    if(i===0){
+        row.append(cell.cloneNode())
+    }else{
+        let newCell = cell.cloneNode()
+        newCell.addEventListener('drop',droppin_handler)
+        row.append(newCell)
+    }
 }
 
 for (let i = 0; i < 12; i++) {
@@ -30,4 +36,34 @@ function generateCode(){
     }
 }
 
+function droppin_handler(event){
+    event.preventDefault();
+    let data = event.dataTransfer.getData('text/plain')
+    event.target.appendChild(document.getElementById(data))
+}
+
+function dragstart_handler(event){
+    event.currentTarget.style.border = '1px dashed black'
+    event.dataTransfer.setData('text/plain', event.target)
+    event.dataTransfer.dropEffect = 'all';
+}
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    const pins = document.getElementsByTagName('pin')
+    for(let pin of pins){
+        pin.addEventListener('dragstart', dragstart_handler)
+    }
+})
+
+window.addEventListener('dragenter', (event)=>{
+    console.log("valig droppable")
+})
+
 generateCode()
+const header = document.getElementsByTagName('th')
+
+for(let i = 0; i<4;i++)
+{
+    header.item(i+1).innerHTML = codePin;
+    header.item(i+1).classList.add(colors[code[i]])
+}
