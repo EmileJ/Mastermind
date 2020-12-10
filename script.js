daa6332bae20e79535882cd686cf25346dc0a0f0
EmileJ <72444841+EmileJ@users.noreply.github.com>
2020-12-10 16:03:04 +0100

    Finished the drag and drop event handlers
    start working on checking values of turn

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-//const code = {}
 const colors = ['red','green','blue','yellow']
-var turn = 1;
-var code = [];
+let turn = 1;
+let code = [];
+let newNode;
 
 const codePin = '&#9864;';
 const indicator = '&#x1f4cd;';
@@ -18,7 +18,8 @@ const notCorrect = document.createElement('notCorrect');
 correctCol.innerHTML = indicator;
 notCorrect.innerHTML = emptyIndicator;
 
-let dropTarget;
+const header = document.getElementsByTagName('th')
+let currentTurn = document.getElementById(`turn${turn}`);
 
 function generateCode(){
     for(var i = 0; i<4; i++){
@@ -34,73 +35,101 @@ window.addEventListener('DOMContentLoaded', ()=>{
     }
 })
 
-function handleDragStartPin(event){
-    this.style.opacity = '0.4';
-    event.dataTransfer.dropEffect = 'copy';
-    console.log('dragStart pin')
-    console.log(event)
-}
-
-function handleDragEndPin(event){
-    this.style.opacity = '1';
-    console.log('dragEnd pin')
-    console.log(event)
-}
-
-function handleDropPin(event){
-    console.log('dropped pin');
-    console.log(event)
-    console.log("check" + table.contains(dropTarget))
-}
-
-function testf(event){
-    console.log(event)
-}
-
-/**
- * drag         - when dragged
- * dragstart    - when dragging starts
- * dragend      - when mouse released or escape
- * dragenter    - when dragged element enters valid drop target
- * dragexit     - when dragged element stops being active draggable
- * dragleave    - when dragged element leaves drop target
- * dragover     - continuously when being dragged (speed depends on element speed)
- * drop         - when draggable is dropped on valid drop target
- */
-
-window.addEventListener('dragenter', (event)=>{
-    event.preventDefault();
-    dropTarget=event.target;
-    // console.log("valid droppable")
-    // console.log(event)
-})
-
-window.addEventListener('dragover', (event)=>{
-    event.preventDefault();
-})
-
-window.addEventListener('drop',handleDropPin)
-
 generateCode()
-const header = document.getElementsByTagName('th')
 
-for(let i = 1; i<=code.length;i++)
-{
+for(let i = 1; i<=code.length;i++){
     header.item(i).innerHTML = codePin;
     header.item(i).classList.add(colors[code[i-1]])
-}
-
-const testelement = document.getElementById(`turn${turn}`);
-for(let i = 0;i<4;i++){
-    if(i===2) testelement.lastElementChild.innerHTML+='<br />';
-    testelement.lastElementChild.appendChild(correctCol.cloneNode(true));
 }
 
 checkBtn.addEventListener('click', checkCurrentRow);
 
 function checkCurrentRow(){
     const row = document.getElementById(`turn${turn}`);
-    let childs = row.children
-    console.log(row)
-    console.log(childs)
+    const cells = row.children
+
+    for(let i = 1;i<5;i++){
+        if(cells.item(i).children.length === 0){
+            console.log('empty cell detected, skipping check')
+            return
+        }
+    }
+
+    for(let i = 0;i<4;i++){
+        if(i===2) currentTurn.lastElementChild.innerHTML+='<br />';
+        currentTurn.lastElementChild.appendChild(correctCol.cloneNode(true));
+    }
+}
+
+function nextRound(){
+    
+}
+
+for(let i = 1; i<5;i++){
+    const cell = currentTurn.children.item(i);
+    cell.addEventListener('dragenter', setDropTarget);
+    cell.addEventListener('dragover', event => event.preventDefault());
+    cell.addEventListener('dragleave', resetDropTarget);
+    cell.addEventListener('drop', DropTarget);
+}
+
+//Event Handlers
+
+function handleDragStartPin(event){
+    event.target.style.opacity = '0.4';
+    event.dataTransfer.dropEffect = 'copy';
+    event.dataTransfer.setData('text/plain', event.target.classList)
+}
+
+function handleDragEndPin(event){
+    event.target.style.opacity = '';
+}
+
+function handleDropPin(event){
+    event.preventDefault();
+}
+
+function setDropTarget(event){
+    event.preventDefault();
+
+    newNode = event.target;
+    if(event.target.tagName && event.target.tagName.toLowerCase() === 'td'){
+        event.target.style.border = "2px dotted black";
+    }else{
+        const cell = event.target.parentElement.parentElement
+        cell.style.border = "2px dotted black";
+    }
+}
+
+function resetDropTarget(event){
+    event.preventDefault();
+
+    if(event.target.contains(newNode)){
+        return
+    }
+    if(event.target.tagName && event.target.tagName.toLowerCase() === 'td'){
+        event.target.style.border = "";
+    }
+    if(!newNode.contains(event.target)){
+        event.target.parentElement.parentElement.style.border = "";
+    }
+}
+
+function DropTarget(event){
+    event.preventDefault();
+    event.target.style.border = "";
+    const data = event.dataTransfer.getData('text/plain')
+    if(event.target.tagName.toLowerCase() === "td" && event.target.children.length === 0){
+        const element = document.createElement('pin');
+        element.innerHTML = codePin;
+        element.classList = data;
+        event.target.appendChild(element);
+    }
+    if(event.target.tagName.toLowerCase() === "td" && event.target.children.length > 0){
+        event.target.firstChild.classList = data;
+    }
+    if(event.target.tagName.toLowerCase() === 'pin'){
+        event.target.parentNode.style.border = '';
+        event.target.classList = data;
+    }
 }
